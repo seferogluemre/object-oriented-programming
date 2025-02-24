f80b6d3f5732ac5476755473e62a233675d8b6ee
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 16:17:38 +0300

    created compeng class and EEE Class

--- polymorphism.py
+++ polymorphism.py
@@ -1,4 +1,22 @@
 class Employee:
-    
-    def raise(self):
-        
+
+    def raise (self):
+        raise_rate = 0.1
+        return 100+100*raise_rate
+
+
+class CompEng(Employee):
+    def raise (self):
+        raise_rate = 0.2
+        return 100+100*raise_rate
+
+
+class EEE(Employee):
+    def raise (self):
+        raise_rate = 0.3
+        return 100+100*raise_rate
+
+
+eee=EEE()
+ce=CompEng()
+employee=Employee()
