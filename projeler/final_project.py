743b8603b7bbc051816080769857d2e788ce0869
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 16:44:11 +0300

    writed child class

--- projeler/final_project.py
+++ projeler/final_project.py
@@ -1,12 +1,20 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
-class Shape:
+
+class Shape(ABC):
     @abstractmethod
-    def area(self):pass
-    
+    def area(self): pass
+
     @abstractmethod
-    def perimeter(self):pass
-    
-    def toString(self):pass
-    
-    
+    def perimeter(self): pass
+
+    def toString(self): pass
+
+
+class Square(Shape):
+
+    def __init__(self, edge):
+        self.__edge = edge
+
+    def area(self):
+        result = self.__edge**2
