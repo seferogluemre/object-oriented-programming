3b785b5633495b706775355a6b2233ea9af0d7ec
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 11:40:58 +0300

    Formatting logging in terminal

--- projeler/calculator/calculator.py
+++ projeler/calculator/calculator.py
@@ -8,10 +8,32 @@ class Calc(object):
     
         
 
-    def add():
+    def add(self):
         return self.value1+self.value2        
     
     
     def multiply(self):
         return self.value1*self.value2
          
+
+
+
+v1=5
+v2=4
+
+c1=Calc(v1, v2)
+add_result=c1.add()
+multiply_result=c1.multiply()
+
+print("Add: {}, Multiply: {}".format(add_result, multiply_result))
+
+
+
+
+
+
+
+
+
+
+
