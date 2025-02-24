c1a782417521f0dfc03323532a4c891276e03d8f
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 13:12:36 +0300

    Creating the bank account class

---
+++ encapsulation.py
@@ -0,0 +1,17 @@
+class BankAccount(object):
+     
+    def __init__(self,name,money,address):
+        self.name=name
+        self.money=money
+        self.address=address
+        
+        
+        
+p1=BankAccount("Ahmet mehmetoglu", 12000, "İstanbul")
+p2=BankAccount("Osman osmanoglu", 7300, "Paris")
+
+print(p1.money)
+
+p1.money+=p2.money
+print(p1.money)
+print(p2.money)
