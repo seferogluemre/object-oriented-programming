11ed50bbc300e4d1cb4b9114a4540a364c6f6c3b
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 11:34:34 +0300

    created Calc class and initilazer

---
+++ projeler/calculator/calculator.py
@@ -0,0 +1,14 @@
+# Calculator Project
+
+class Calc(object):
+    
+    def __init__(self):
+        #Attribute
+        pass
+    
+    def add():
+        pass
+    
+    def multiply(self):
+        
+        
