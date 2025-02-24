e6e7e125720a3d20757785207570f5a98bd2eb5e
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 13:52:30 +0300

    Creating and referencing the child object

--- projeler/inheritence-project/inheritence_project.py
+++ projeler/inheritence-project/inheritence_project.py
@@ -1,21 +1,23 @@
 class WebSite:
-    
-    def __init__(self,name,surname):
-        self.name=name
-        self.surname=surname
-        
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
     def login_info(self):
         print(self.name+" "+self.surname)
+
+
+class WebSite1(WebSite):
+    def __init__(self, name, surname,ids):
+        WebSite.__init__(self, name, surname)
+        self.ids=ids
         
-    
+    def login(self):
+        print(self.name+" "+self.surname+" "+self.ids)
 
 
 
 
-p1=WebSite("Ahmet", "Mehmetoglu")
+p1 = WebSite("Ahmet", "Mehmetoglu")
 p1.login_info()
-
-
-
-
-
