77a6db48ec20bb6cd715442583d74c11e32e039b
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 16:19:52 +0300

    completion of the polymorphism example

--- polymorphism.py
+++ polymorphism.py
@@ -2,21 +2,27 @@ class Employee:
 
     def raise (self):
         raise_rate = 0.1
-        return 100+100*raise_rate
+        result = 100+100*raise_rate
+        print("Employee: "+result)
 
 
 class CompEng(Employee):
     def raise (self):
         raise_rate = 0.2
-        return 100+100*raise_rate
+        result = 100+100*raise_rate
+        print("Comp Eng: "+result)
 
 
 class EEE(Employee):
     def raise (self):
         raise_rate = 0.3
-        return 100+100*raise_rate
+        result = 100+100*raise_rate
+        print("EEE: "+result)
 
 
-eee=EEE()
-ce=CompEng()
-employee=Employee()
+eee = EEE()
+ce = CompEng()
+employee = Employee()
+
+for employee in employee_list:
+    employee.raise()
