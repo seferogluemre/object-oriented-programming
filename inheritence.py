05e6109b070e37dd66d6518c56a7833c39c006ac
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 13:36:07 +0300

    additional functions were written

--- inheritence.py
+++ inheritence.py
@@ -9,8 +9,15 @@ class Animal:
     def walk(self):
         print("Animal walk")
         
+        
 
 class Monkey(Animal()):
     def __init__(self):
         super().__init__() # Referans alınan nesnenin initilaze fonksiyonunu kullanabiliyor artık
-             
+        print("Monkey created")
+        
+    def toString(self):
+        print("Monkey")
+        
+    def climb(self):
+        print("Monkey can climb")
