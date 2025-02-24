998b3566ab02ef96b4fd979ca5a6da2f6db673fc
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 13:33:08 +0300

    Creating the parent object and writing basic functions

---
+++ inheritence.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self):
+        print("Animal is created")
+        
+        
+    def toString(self):
+        print("Animal")
+        
+    def walk(self):
+        print("Animal walk")
+        
