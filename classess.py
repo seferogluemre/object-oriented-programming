07f0a2640d985b4879daf798a636b74d004eb04e
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-02-24 10:34:54 +0300

    created new footballer class

--- classess.py
+++ classess.py
@@ -1,10 +1,22 @@
-employee1_name="Emre"
-employee1_age=17
-employee1_address="Rize"
+employee1_name = "Emre"
+employee1_age = 17
+employee1_address = "Rize"
+
 
 class Employee(object):
-    
-    pass 
-        
 
-employee_one=Employee()
+    pass
+
+
+employee_one = Employee()
+
+
+class Footballer:
+
+    age = 30
+    football_club="Barcelona"
+    
+    
+f1=Footballer()
+
+print(f1)
